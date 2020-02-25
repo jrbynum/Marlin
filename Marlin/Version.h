aaa61bb73944bf5d15debb58b6d0d1ecac0a939e
Rusty Bynum <jrbynum@shaw.ca>
2020-02-25 15:59:02 -0700

    Swapped Serial Ports
    Changed primary to -1 and secondary to 0

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "frankienstein-2.0.1"
+#define SHORT_BUILD_VERSION "frankienstein-2.0.2"
 
 /**
  * Verbose version identifier which should contain a reference to the location
